49c474b1e7ff66eea85f26bec53ca93d1ef5c1c6
borjafg <borjafg_95@hotmail.com>
2016-04-11 18:22:57 +0200

    Primer acercamiento a una solución para mostrar gráficos y poder añadir otros nuevos

---
+++ src/main/java/es/uniovi/asw/voterResult/request/OpcionesMostrarResultados.java
@@ -0,0 +1,8 @@
+package es.uniovi.asw.voterResult.request;
+
+
+public class OpcionesMostrarResultados
+{
+	// Habrá que comtemplar las posibles opciones que puede haber teniendo en cuenta
+	// que puede cambiar el tipo de gráfico, pueden aparecer nuevas elecciones
+}
