5a43adbfd3a40e0ba8320cdb7274b1780c070ca4
Nauce <naucelg@hotmail.com>
2016-04-12 23:18:00 +0200

    Modelo de dominio JPA V1.1.1

--- src/main/java/es/uniovi/asw/dbManagement/model/Election.java
+++ src/main/java/es/uniovi/asw/dbManagement/model/Election.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,150 +14,114 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
 
-
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public abstract class Election
-{
-	@Id @GeneratedValue private Long id;
+public abstract class Election {
+
+	@Id @GeneratedValue
+	private Long id;
+	@Column(unique = true)
 	private String name;
 	private Date startDate;
 	private Date expiryDate;
 	private String instructions;
+	private int numChoices = 1;
 	
 	@OneToMany(mappedBy = "election", fetch = FetchType.EAGER)
 	private Set<Vote> votes = new HashSet<>();
-	
-	@OneToMany(mappedBy = "election", fetch = FetchType.EAGER)
+	@OneToMany(mappedBy = "election", fetch = FetchType.EAGER, cascade={CascadeType.ALL})
 	private Set<Turnout> turnout = new HashSet<>();
 	
-	
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
-	
-	public void setName(String name)
-	{
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Date getStartDate()
-	{
+
+	public Date getStartDate() {
 		return startDate;
 	}
-	
-	public void setStartDate(Date startDate)
-	{
+
+	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
-	
-	public Date getExpiryDate()
-	{
+
+	public Date getExpiryDate() {
 		return expiryDate;
 	}
-	
-	public void setExpiryDate(Date expiryDate)
-	{
+
+	public void setExpiryDate(Date expiryDate) {
 		this.expiryDate = expiryDate;
 	}
-	
-	public String getInstructions()
-	{
+
+	public String getInstructions() {
 		return instructions;
 	}
 
-	public void setInstructions(String instructions)
-	{
+	public void setInstructions(String instructions) {
 		this.instructions = instructions;
 	}
 	
-	public Set<Vote> getVotes()
-	{
+	public int getNumChoices() {
+		return numChoices;
+	}
+
+	public void setNumChoices(int numChoices) {
+		this.numChoices = numChoices;
+	}
+
+
+	public Set<Vote> getVotes() {
 		return new HashSet<>(votes);
 	}
 	
-	Set<Vote> _getVotes()
-	{
+	Set<Vote> _getVotes() {
 		return votes;
 	}
-	
-	public Set<Turnout> getTurnout()
-	{
+
+	public Set<Turnout> getTurnout() {
 		return new HashSet<>(turnout);
 	}
 	
-	Set<Turnout> _getTurnout()
-	{
+	Set<Turnout> _getTurnout() {
 		return turnout;
 	}
-	
-	public Long getId()
-	{
+
+	public Long getId() {
 		return id;
 	}
-	
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "Election [id=" + id + ", name=" + name + ", startDate=" + startDate + ", expiryDate=" + expiryDate
 				+ ", instructions=" + instructions + "]";
 	}
-	
+
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		
-		result = prime * result + ((expiryDate == null) ? 0 : expiryDate.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
-		
 		return result;
 	}
-	
+
 	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		
 		if (obj == null)
 			return false;
-		
 		if (getClass() != obj.getClass())
 			return false;
-		
 		Election other = (Election) obj;
-		
-		if (expiryDate == null)
-		{
-			if (other.expiryDate != null)
-				return false;
-		}
-		
-		else if (!expiryDate.equals(other.expiryDate))
-			return false;
-		
-		if (name == null)
-		{
+		if (name == null) {
 			if (other.name != null)
 				return false;
-		}
-		
-		else if (!name.equals(other.name))
+		} else if (!name.equals(other.name))
 			return false;
-		
-		if (startDate == null)
-		{
-			if (other.startDate != null)
-				return false;
-		}
-		
-		else if (!startDate.equals(other.startDate))
-			return false;
-		
 		return true;
 	}
+	
 }
