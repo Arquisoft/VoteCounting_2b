45c59b645e049f6106b89489114196276b34fab7
marcelotm23 <marcelo.torrejn@gmail.com>
2016-04-20 17:30:34 +0200

    Revert "Factorización de código"
    This reverts commit 0ae6661b8f0f9de47eadbfdbad79a5338d88a385.

--- src/test/java/es/uniovi/asw/seleniumTests/Principal.java
+++ src/test/java/es/uniovi/asw/seleniumTests/Principal.java
@@ -1,12 +1,27 @@
 package es.uniovi.asw.seleniumTests;
 
-import static org.junit.Assert.assertEquals;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
+public class Principal {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
 
-public class Principal extends Factory{
- 
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
   @Test
   public void testPrincipal() throws Exception {
     driver.get(baseUrl + "/principal.xhtml");
@@ -38,5 +53,45 @@ public class Principal extends Factory{
     driver.findElement(By.id("j_idt12")).click();
   }
 
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
 
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
 }
