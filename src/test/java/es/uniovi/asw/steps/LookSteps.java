60ca0674c306a59b626fff742dc882aec4780a37
marcelotm23 <marcelo.torrejn@gmail.com>
2016-04-20 19:03:21 +0200

    Arreglos para borja :8ball:

--- src/test/java/es/uniovi/asw/steps/LookSteps.java
+++ src/test/java/es/uniovi/asw/steps/LookSteps.java
@@ -4,11 +4,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import java.util.concurrent.TimeUnit;
 
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationContextLoader;
@@ -26,105 +25,68 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Entonces;
+import cucumber.api.java.es.Y;
 import es.uniovi.asw.Factory;
 import es.uniovi.asw.Main;
 
-@ContextConfiguration(classes=Main.class, loader=SpringApplicationContextLoader.class)
+@ContextConfiguration(classes = Main.class, loader = SpringApplicationContextLoader.class)
 @IntegrationTest
 @WebAppConfiguration
-public class LookSteps extends Factory{
+public class LookSteps extends Factory {
 	@Autowired
-	  protected WebApplicationContext context;
-	  protected MockMvc mvc;
-	  protected MvcResult result;
-	  private WebDriver driver;
-	  private String baseUrl;
+	protected WebApplicationContext context;
+	protected MockMvc mvc;
+	protected MvcResult result;
+	private WebDriver driver;
+	private String baseUrl;
 
-	  @Before
-	  public void setUp() throws Exception {
-	    driver = new FirefoxDriver();
-	    baseUrl = "http://localhost:8080";
-	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  }
-	
-	
-	  //Scenario 5
-	  @When("^the client click on the link \"([^\"]*)\"$")
-	  public void the_client_click_on_the_link(String str) throws Throwable {
-		  System.out.println("the client click on the link "+str);
-		  driver.get(baseUrl + "/principal.xhtml");
-		  assertEquals(str, driver.findElement(By.linkText(str)).getText());
-		  driver.findElement(By.id("j_idt8")).click();
-		  
-	  }
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://localhost:8080";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-	  @Then("^the client makes call to Lista Elecciones GET /listaElecciones\\.xhtml$$")
-	  public void the_client_makes_call_to_Lista_Elecciones_GET() throws Throwable {
-		  Assert.notNull(context);
-		  this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
-		  result = mvc.perform(get("/listaElecciones.xhtml")).andReturn();
-		  driver.get(baseUrl + "/listaElecciones.xhtml");
-	  }
-	  
-	  @Then("^the client receives Lista Elecciones status code of (\\d+)$")
-	  public void the_client_receives_Lista_Elecciones_status_code_of(int stat) throws Throwable {
-		  assertThat(result.getResponse().getStatus(), is(stat));
-	  }
-	  
-	  //Scenario 6, 7, 8
-	  @When("^the client choose \"([^\"]*)\" with id \"([^\"]*)\"$")
-	  public void the_client_choose(String str, String id) throws Throwable {
-		 System.out.println("the client choose "+str);
-		 driver.get(baseUrl + "/principal.xhtml");
-		 assertEquals(str, driver.findElement(By.linkText(str)).getText());
-		 driver.findElement(By.id("dtLj_idt9")).click();
-		 driver.findElement(By.linkText(str)).click();
-		 driver.get(baseUrl + "/listaElecciones.xhtml");
-		 assertEquals(str, driver.findElement(By.linkText(str)).getText());
-		 driver.findElement(By.id("dtLj_idt9")).click();
-		 driver.findElement(By.linkText(str)).click();
-	  }
+	// Scenario 5
 
-	  @Then("^the client makes call to Referendum GET /listaReferendum\\.xhtml$$")
-	  public void the_client_makes_call_to_Referendum_GET() throws Throwable {
-		  Assert.notNull(context);
-		  this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
-		  result = mvc.perform(get("/listaReferendum.xhtml")).andReturn();
-	  }
-	  
-	  @Then("^the client receives Referendum status code of (\\d+)$")
-	  public void the_client_receives_Referendum_status_code_of(int stat) throws Throwable {
-		  assertThat(result.getResponse().getStatus(), is(stat));
-	  }
-	  
-	  @Then("^the client makes call to Listas Cerradas GET /listaCerradas\\.xhtml$$")
-	  public void the_client_makes_call_to_Listas_Cerradas_GET() throws Throwable {
-		  Assert.notNull(context);
-		  this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
-		  result = mvc.perform(get("/listaCerradas.xhtml")).andReturn();
-	  }
-	  
-	  @Then("^the client receives Listas Cerradas status code of (\\d+)$")
-	  public void the_client_receives_Listas_Cerradas_status_code_of(int stat) throws Throwable {
-		  assertThat(result.getResponse().getStatus(), is(stat));
-	  }
-	  
-	  @Then("^the client makes call to Listas Abiertas GET /listaAbiertas\\.xhtml$$")
-	  public void the_client_makes_call_to_Listas_Abiertas_GET() throws Throwable {
-		  Assert.notNull(context);
-		  this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
-		  result = mvc.perform(get("/listaAbiertas.xhtml")).andReturn();
-	  }
-	  
-	  //De momento se comprueba en el landing que devuelve 404
-	  @Then("^the client receives Listas Abiertas status code of (\\d+)$")
-	  public void the_client_receives_Listas_Abiertas_status_code_of(int stat) throws Throwable {
-		  assertThat(result.getResponse().getStatus(), is(stat));
-	  }
-	  
-	  @After
-	  public void tearDown() throws Exception {
-	    driver.quit();
-	   
-	  }
+	@Entonces("^el cliente hace click en \"([^\"]*)\"$")
+	public void el_cliente_hace_click_en(String str) throws Throwable {
+		driver.get(baseUrl + "/principal.xhtml");
+		driver.findElement(By.linkText(str)).click();
+	}
+
+	@Y("^el cliente hace click en las diferentes opciones de la tabla")
+	public void el_cliente_hace_click_en_las_diferentes_opciones_de_la_tabla() throws Throwable {
+		driver.findElement(By.cssSelector("th.sorting_asc")).click();
+		driver.findElement(By.cssSelector("th.sorting")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[3]")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[4]")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[5]")).click();
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("25");
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("50");
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("100");
+		driver.findElement(By.cssSelector("input.form-control.input-sm")).clear();
+	}
+
+	@Y("^selecciona una eleccion de la que quiere saber los resultados")
+	public void selecciona_una_eleccion_de_la_que_quiere_saber_los_resultados() throws Throwable {
+		driver.findElement(By.cssSelector("th.sorting_asc")).click();
+		driver.findElement(By.cssSelector("th.sorting")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[3]")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[4]")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[5]")).click();
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("25");
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("50");
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("100");
+		driver.findElement(By.cssSelector("input.form-control.input-sm")).clear();
+		driver.findElement(By.cssSelector("input.form-control.input-sm")).sendKeys("cataluña");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+
+	}
 }
--- src/test/resources/features/look.feature
+++ src/test/resources/features/look.feature
@@ -1,21 +1,7 @@
 # language: es
- Feature: look page
-  Scenario: the client want have a look of "Lista Elecciones"
-  	When the client click on the link "Lista Elecciones"
-  	Then the client makes call to Lista Elecciones GET /listaElecciones.xhtml
-  	And the client receives Lista Elecciones status code of 200
-  	
-  Scenario: the client want see "Referendum" of "Tipos de Elecciones"
-  	When the client choose "Referendum" with id "j_idt10"
-  	Then the client makes call to Referendum GET /listaReferendum.xhtml
-  	And the client receives Referendum status code of 200
-  
-  Scenario: the client want see "Listas Cerradas" of "Tipos de Elecciones"
-  	When the client choose "Listas Cerradas" with id "j_idt11"
-  	Then the client makes call to Listas Cerradas GET /listaCerradas.xhtml
-  	And the client receives Listas Cerradas status code of 200
-  	
-  Scenario: the client want see "Listas Abiertas" of "Tipos de Elecciones"
-  	When the client choose "Listas Abiertas" with id "j_idt12"
-  	Then the client makes call to Listas Abiertas GET /listaAbiertas.xhtml
-  	And the client receives Listas Abiertas status code of 404
+  Característica: Lista Elecciones
+   Escenario: Acceso a Lista de Elecciones
+     Cuando el cliente entra en la web /principal.xhtml
+     Entonces el cliente hace click en "Lista Elecciones"
+     Y el cliente hace click en las diferentes opciones de la tabla
+     Y selecciona una eleccion de la que quiere saber los resultados
