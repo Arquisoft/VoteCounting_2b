885f179f0c07e605368cc8c2c261d50cc53bbe44
Sergio Moradas <sergimoradas@gmail.com>
2016-04-20 17:05:57 +0200

    Prueba

--- src/test/java/es/uniovi/asw/steps/LandingSteps.java
+++ src/test/java/es/uniovi/asw/steps/LandingSteps.java
@@ -16,7 +16,6 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import es.uniovi.asw.Main;
