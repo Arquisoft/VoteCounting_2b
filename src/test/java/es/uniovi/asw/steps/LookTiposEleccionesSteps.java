47bfe2a2d6799c7caa49639c231ea535d5241959
Sergio Moradas <sergimoradas@gmail.com>
2016-04-21 18:13:36 +0200

    Cambios test

--- src/test/java/es/uniovi/asw/steps/LookTiposEleccionesSteps.java
+++ src/test/java/es/uniovi/asw/steps/LookTiposEleccionesSteps.java
@@ -3,42 +3,53 @@ package es.uniovi.asw.steps;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Y;
 import es.uniovi.asw.util.SeleniumUtils;
 
 public class LookTiposEleccionesSteps {
-	
-	protected WebDriver driver = SeleniumUtils.getDriver();
-	
-	  //Scenario 3, 4, 5
-	  @Y("^el cliente selecciona \"([^\"]*)\"$")
-	  public void el_cliente_selecciona(String str) throws Throwable {
-		  driver.get("http://localhost:8080" + "/lista"+str+".xhtml");
-		  
-	  }
 
-	  @Y("^el cliente ve la tabla de con las respectivas elecciones de tipo \"([^\"]*)\"$")
-	  public void el_cliente_ve_la_tabla_de_con_las_respectivas_elecciones_de_tipo(String tipo) throws Throwable {
-		  	new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("25");
-		    new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("50");
-		    new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("100");
-		    driver.findElement(By.cssSelector("th.sorting_asc")).click();
-		    driver.findElement(By.cssSelector("th.sorting")).click();
-		    driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[3]")).click();
-		    driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[4]")).click();
-		    driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[5]")).click();
-		    driver.findElement(By.cssSelector("input.form-control.input-sm")).clear();
-	  }
-	  
-	  @Y("^el cliente elije aquella que sea de su interés")
-	  public void el_cliente_elije_aquella_que_sea_de_su_interes() throws Throwable {
-//		  driver.findElement(By.id("bElection:1:j_idt29:j_idt30")).click();
-//		    new Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("25");
-//		    new Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("50");
-//		    new Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("100");
-//		    driver.findElement(By.cssSelector("th.sorting_asc")).click();
-//		    driver.findElement(By.cssSelector("th.sorting")).click();
-		  
-		  SeleniumUtils.finishTest(driver);
-	  }
+	protected WebDriver driver = SeleniumUtils.getDriver();
+
+	// Scenario 3, 4, 5
+
+	@Cuando("^el cliente entra en la web \"([^\"]*)\"$")
+	public void el_cliente_entra_en_la_web(String arg1) throws Throwable {
+		driver.get("http://localhost:8080" + arg1);
+	}
+
+	@Y("^el cliente selecciona \"([^\"]*)\"$")
+	public void el_cliente_selecciona(String str) throws Throwable {
+		
+
+	}
+
+	@Y("^el cliente ve la tabla de con las respectivas elecciones de tipo \"([^\"]*)\"$")
+	public void el_cliente_ve_la_tabla_de_con_las_respectivas_elecciones_de_tipo(String tipo) throws Throwable {
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("25");
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("50");
+		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("100");
+		driver.findElement(By.cssSelector("th.sorting_asc")).click();
+		driver.findElement(By.cssSelector("th.sorting")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[3]")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[4]")).click();
+		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[5]")).click();
+		driver.findElement(By.cssSelector("input.form-control.input-sm")).clear();
+	}
+
+	@Y("^el cliente elije aquella que sea de su interés")
+	public void el_cliente_elije_aquella_que_sea_de_su_interes() throws Throwable {
+		// driver.findElement(By.id("bElection:1:j_idt29:j_idt30")).click();
+		// new
+		// Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("25");
+		// new
+		// Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("50");
+		// new
+		// Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("100");
+		// driver.findElement(By.cssSelector("th.sorting_asc")).click();
+		// driver.findElement(By.cssSelector("th.sorting")).click();
+
+		SeleniumUtils.finishTest(driver);
+	}
 }
--- src/test/resources/features/looktiposelecciones.feature
+++ src/test/resources/features/looktiposelecciones.feature
@@ -1,22 +1,16 @@
 # language: es
  Característica: Tipos de elecciones
-   Escenario: Acceso a Tipos Elecciones: Referendum
-     Cuando el cliente entra en la web /principal.xhtml
-     Entonces el cliente hace click en "Tipos Elecciones"
-     Y el cliente selecciona "Referendum"
-     Y el cliente ve la tabla de con las respectivas elecciones de tipo "Referendum"
-     Y el cliente elije aquella que sea de su interés
-
-	 Escenario: Acceso a Tipos Elecciones: Listas Cerradas
-     Cuando el cliente entra en la web /principal.xhtml
-     Entonces el cliente hace click en "Tipos Elecciones"
-     Y el cliente selecciona "Cerradas"
-     Y el cliente ve la tabla de con las respectivas elecciones de tipo "Listas Cerradas"
+   Escenario: Acceso a Tipos Elecciones: Listas Abiertas
+     Cuando el cliente entra en la web "/listasAbiertas.xhtml"
+     Entonces el cliente ve la tabla de con las respectivas elecciones de tipo "Listas Abiertas"
+     Y el cliente elije aquella que sea de su interés 
+     
+   Escenario: Acceso a Tipos Elecciones: Listas Cerradas
+     Cuando el cliente entra en la web "/listaCerradas.xhtml"
+     Entonces el cliente ve la tabla de con las respectivas elecciones de tipo "Listas Cerradas"
      Y el cliente elije aquella que sea de su interés
      
-   Escenario: Acceso a Tipos Elecciones: Listas Abiertas
-     Cuando el cliente entra en la web /principal.xhtml
-     Entonces el cliente hace click en "Tipos Elecciones"
-     Y el cliente selecciona "sAbiertas"
-     Y el cliente ve la tabla de con las respectivas elecciones de tipo "Listas Abiertas"
+    Escenario: Acceso a Tipos Elecciones: Referendum
+     Cuando el cliente entra en la web "/listaReferendum.xhtml"
+     Entonces el cliente ve la tabla de con las respectivas elecciones de tipo "Referendum"
      Y el cliente elije aquella que sea de su interés
