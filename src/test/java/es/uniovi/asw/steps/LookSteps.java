14dff51bcc79c87ea9650de04223fd1d86bcc17f
marcelotm23 <marcelo.torrejn@gmail.com>
2016-04-21 18:08:06 +0200

    Modificaciones

--- src/test/java/es/uniovi/asw/steps/LookSteps.java
+++ src/test/java/es/uniovi/asw/steps/LookSteps.java
@@ -30,29 +30,25 @@ public class LookSteps extends Factory {
 
 	@Y("^el cliente hace click en las diferentes opciones de la tabla")
 	public void el_cliente_hace_click_en_las_diferentes_opciones_de_la_tabla() throws Throwable {
-		driver.findElement(By.cssSelector("th.sorting_asc")).click();
-		driver.findElement(By.cssSelector("th.sorting")).click();
-		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[3]")).click();
-		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[4]")).click();
-		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[5]")).click();
 		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("25");
-		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("50");
-		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("100");
-		driver.findElement(By.cssSelector("input.form-control.input-sm")).clear();
+	    new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("50");
+	    new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("100");
+	    driver.findElement(By.cssSelector("th.sorting_asc")).click();
+	    driver.findElement(By.cssSelector("th.sorting")).click();
+	    driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[3]")).click();
+	    driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[4]")).click();
+	    driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[5]")).click();
 		
 	}
 
 	@Y("^selecciona una eleccion de la que quiere saber los resultados")
 	public void selecciona_una_eleccion_de_la_que_quiere_saber_los_resultados() throws Throwable {
-		driver.findElement(By.cssSelector("th.sorting_asc")).click();
-		driver.findElement(By.cssSelector("th.sorting")).click();
-		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[3]")).click();
-		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[4]")).click();
-		driver.findElement(By.xpath("//table[@id='bElection']/thead/tr/th[5]")).click();
-		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("25");
-		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("50");
-		new Select(driver.findElement(By.name("bElection_length"))).selectByVisibleText("100");
-		driver.findElement(By.cssSelector("input.form-control.input-sm")).clear();
+		driver.findElement(By.id("bElection:0:j_idt30:j_idt31")).click();
+	    new Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("25");
+	    new Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("50");
+	    new Select(driver.findElement(By.name("bcarPool_length"))).selectByVisibleText("100");
+	    driver.findElement(By.cssSelector("th.sorting_asc")).click();
+	    driver.findElement(By.cssSelector("th.sorting")).click();
 		driver.findElement(By.cssSelector("input.form-control.input-sm")).sendKeys("cataluña");
 		
 		 SeleniumUtils.finishTest(driver);
